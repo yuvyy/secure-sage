87de6c410e3509e7c9563a11893a9e530108dc75
Inferno565 <hamdulesharif555@gmail.com>
2024-09-07 20:34:31 +0530

    Frontend Logics

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -3,8 +3,32 @@ import TotalPieChart from "./totalPieChart";
 import { Button } from "./ui/button";
 import { Plus, Repeat } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Dashboard() {
+  const [items, setItems] = useState([]);
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
+  var Number_systems;
+
+  //add api route here
+  useEffect(() => {
+    fetch("")
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setDataIsLoaded(true);
+        // Number_systems = items.systems.length
+      });
+  }, []);
+
+  if (!dataIsLoaded) {
+    return (
+      <div>
+        <h1>Please wait some time...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-primary text-3xl font-semibold">Dashboard</h2>
@@ -12,30 +36,27 @@ export default function Dashboard() {
         <div className="bg-accent shadow-md rounded-lg p-5 flex justify-around items-center flex-grow">
           <div className="flex flex-col gap-4">
             <h3 className="text-accent-foreground font-medium text-4xl">
-              This is a big test
+              {items.name}
             </h3>
             <ul className="pl-6 flex flex-col gap-1">
               <li className="text-muted-foreground font-medium">
-                Date: 09/05/2024
+                Date: {items.date}
               </li>
               <li className="text-muted-foreground font-medium">
-                Time: 10:25 pm
+                Time: {items.time}
               </li>
-              <li className="text-muted-foreground font-medium">
-                Systems: pc1,pc2,pc3
-              </li>
+              {/* <li className="text-muted-foreground font-medium">Systems: {items.systems.join(",")}</li> */}
             </ul>
           </div>
           <div>
-            <TotalPieChart pass={100} fail={200} />
-             {/* data tp be fetched from db */}
+            <TotalPieChart pass={items.pass} fail={items.fail} />
           </div>
         </div>
         <div className="flex flex-col gap-4">
           <div className="bg-accent rounded-lg h-max p-5 shadow-md flex-grow">
             <h3 className="text-3xl font-semibold">Available Systems</h3>
             <div className="text-9xl text-primary text-center font-semibold">
-              69
+              {Number_systems}
             </div>
           </div>
           <div className="flex gap-4 self-center flex-grow">
--- src/Components/totalPieChart.jsx
+++ src/Components/totalPieChart.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { TrendingUp } from "lucide-react";
-import { Label, Pie, PieChart } from "recharts";
+
+import React from "react"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
 
 import {
   Card,
@@ -9,14 +10,15 @@ import {
   CardFooter,
   // CardHeader,
   // CardTitle,
-} from "@/components/ui/card";
+} from "@/components/ui/card"
 import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart";
+} from "@/components/ui/chart"
 
-export const description = "A donut chart with text";
+// export const description = "A donut chart with text"
+
 
 const chartConfig = {
   visitors: {
@@ -29,74 +31,60 @@ const chartConfig = {
   safari: {
     label: "Fail",
     color: "hsl(var(--destructive))",
-  },
-};
+  }
+}
 
-export default function TotalPieChart({ pass, fail }) {
-  const chartData = [
-    { result: "pass", number: pass, fill: "var(--color-chrome)" },
-    { result: "fail", number: fail, fill: "var(--color-safari)" },
+export default function TotalPieChart({pass,fail}) {
+
+const chartData = [
+  { result: "pass", number: pass, fill: "var(--color-chrome)" },
+  { result: "fail", number: fail, fill: "var(--color-safari)" },
   ];
-
-  const totalTests = React.useMemo(() => {
-    return chartData.reduce((acc, curr) => acc + curr.number, 0);
-  }, []);
+    const totalTests = React.useMemo(() => {
+      return chartData.reduce((acc, curr) => acc + curr.number, 0);
+    }, []);
   return (
     <Card className="flex flex-col">
       <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]">
+        <ChartContainer config={chartConfig} className="mx-auto aspect-square max-h-[250px]">
           <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
             <Pie
               data={chartData}
               dataKey="number"
               nameKey="result"
               innerRadius={65}
-              strokeWidth={5}>
+              strokeWidth={5}
+            >
               <Label
                 content={({ viewBox }) => {
                   if (viewBox && "cx" in viewBox && "cy" in viewBox) {
                     return (
-                      <text
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        textAnchor="middle"
-                        dominantBaseline="middle">
-                        <tspan
-                          x={viewBox.cx}
-                          y={viewBox.cy}
-                          className="fill-foreground text-3xl font-bold">
+                      <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle" dominantBaseline="middle">
+                        <tspan x={viewBox.cx} y={viewBox.cy} className="fill-foreground text-3xl font-bold">
                           {totalTests.toLocaleString()}
                         </tspan>
-                        <tspan
-                          x={viewBox.cx}
-                          y={viewBox.cy + 24}
-                          className="fill-muted-foreground">
+                        <tspan x={viewBox.cx} y={viewBox.cy + 24} className="fill-muted-foreground">
                           Tests
                         </tspan>
                       </text>
-                    );
+                    )
                   }
-                  return null;
+                  return null
                 }}
               />
             </Pie>
           </PieChart>
         </ChartContainer>
       </CardContent>
-      {/* <CardFooter className="flex-col gap-2 text-sm">
+      <CardFooter className="flex-col gap-2 text-sm">
         <div className="flex items-center gap-2 font-medium leading-none">
           Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
         </div>
         <div className="leading-none text-muted-foreground">
           Showing total visitors for the last 6 months
         </div>
-      </CardFooter> */}
+      </CardFooter>
     </Card>
-  );
+  )
 }
